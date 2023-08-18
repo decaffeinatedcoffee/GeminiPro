896a210eb6f752ff535043be44f90cdb6d710719
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2023-08-18 15:44:54 -0300

    Revert "Add files via upload"
    This reverts commit 765585999a4badb6f0402b3a2b619f408430a5e1.

--- MCU/SinricHandler.ino
+++
@@ -1,6 +0,0 @@
-bool onPowerState(const String &deviceId, bool &state) {
-  if(state != lastGstate){
-  changeDeviceState();    
-  }        
-  return true;                              
-}
--- MCU/pirRead.ino
+++
@@ -1,9 +0,0 @@
-void readPIR(){
-  if(digitalRead(D8) == HIGH){
-    if(lastPir != 1){
-    Serial.println("[SYSTEM] Motion detected.");
-    lastPir = 1;
-    postServer();
-  }
-  }
-}
